58e77cc69e16ec0ef114e011ccfaa08e2f55b734
Rustem Sayargaliev <r.sayargaliev@gmail.com>
2018-04-16 21:56:25 +0200

    Change availability logic a bit, add healthcheck

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from time import sleep
 
-from requests import HTTPError
+import requests
 from requests_html import HTML, HTMLSession
 from telegram import Bot
 
@@ -9,6 +9,7 @@ BASE_URL = 'http://serienplakate.de/'
 SLEEP_TIME = os.environ.get('SLEEP_TIME', 10 * 60)
 TELEGRAM_BOT_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
 TELEGRAM_USER_ID = os.environ.get('TELEGRAM_USER_ID')
+HEALTHCHECK_TOKEN = os.environ.get('HEALTHCHECK_TOKEN')
 
 
 class Parser:
@@ -21,7 +22,8 @@ class Parser:
         about = response.html.find('.categorie-content', first=True)
         return [p.attrs['data-sid'] for p in about.find('.item')]
 
-    def check_poster_availability(self, poster_id):
+    def check_poster_availability(self, poster_id) -> int:
+        """Check if there are available posters to order. Method returns quantity."""
         response = self.session.post(
             '{}backend/_ajax.php'.format(BASE_URL),
             data={'cmd': 'poster', 'sId': poster_id}
@@ -29,15 +31,15 @@ class Parser:
 
         try:
             response.raise_for_status()
-        except HTTPError as e:
+        except requests.HTTPError as e:
             self.send_telegram_message(message='{} errored:{}'.format(BASE_URL, e))
             raise
 
         html = HTML(html=response.json()['data'])
-        buttons = html.find('.buttons')
-        to_download_elements = [b.find('.download') for b in buttons]
-        to_order_elements = [b.find('.noselect') for b in buttons]
-        return len(to_download_elements) != len(to_order_elements)
+        quantity_block = html.find('.count')[0]
+        quantity_proportion = quantity_block.text.split(' ')[0]
+        quantity_available = int(quantity_proportion.split('/')[0])
+        return quantity_available
 
     def send_telegram_message(self, message):
         bot = Bot(TELEGRAM_BOT_TOKEN)
@@ -45,7 +47,7 @@ class Parser:
 
     def run(self):
         poster_ids = self.get_poster_ids()
-        order_available = any([self.check_poster_availability(pid) for pid in poster_ids])
+        order_available = any([self.check_poster_availability(pid) > 0 for pid in poster_ids])
         if order_available:
             message = 'I\'ve found a free poster to order at {}'.format(BASE_URL)
             self.send_telegram_message(message=message)
@@ -57,4 +59,6 @@ if __name__ == '__main__':
 
     while True:
         parser.run()
-        sleep(SLEEP_TIME)
+        sleep(int(SLEEP_TIME))
+        if HEALTHCHECK_TOKEN:
+            requests.get('https://hchk.io/{}'.format(HEALTHCHECK_TOKEN))
